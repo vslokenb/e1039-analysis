591446a3cf17df245cec320491a1c0d235a875ce
Kenichi Nakano <knakano@nucl.phys.titech.ac.jp>
2024-04-25 12:41:39 -0500

    Updated the setup script.

--- EvalJpsiAsymStat/setup.sh
+++ EvalJpsiAsymStat/setup.sh
@@ -2,8 +2,7 @@ DIR_TOP=$(dirname $(readlink -f $BASH_SOURCE))
 DIR_BUILD=$DIR_TOP/build
 DIR_INST=$DIR_TOP/inst
 
-#source /exp/seaquest/app/software/osg/software/e1039/this-e1039.sh
-source /exp/seaquest/app/software/osg/users/kenichi/e1039/core-20220104/this-e1039.sh
+source /exp/seaquest/app/software/osg/software/e1039/this-e1039.sh
 #source /exp/seaquest/app/software/osg/users/$USER/e1039/core/this-e1039.sh
 export   LD_LIBRARY_PATH=$DIR_INST/lib:$LD_LIBRARY_PATH
 export ROOT_INCLUDE_PATH=$DIR_TOP/inst/include:$ROOT_INCLUDE_PATH
